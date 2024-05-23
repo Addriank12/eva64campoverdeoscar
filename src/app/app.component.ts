96fc0fb28ee94f7660e9f8a1ec05735167ff5838
Adrian <addriank@outlook.com>
2024-05-23 07:29:56 -0500

    7:29 commit inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './Components/header/header.component';
+import { BodyComponent } from './Components/body/body.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, BodyComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp } from "firebase/app";
 
 import { routes } from './app.routes';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyA10XJ6N6Cwy1SDx5MxiqR6cbawGcBN08k",
+  authDomain: "eva64campoverdeoscar.firebaseapp.com",
+  projectId: "eva64campoverdeoscar",
+  storageBucket: "eva64campoverdeoscar.appspot.com",
+  messagingSenderId: "23194165894",
+  appId: "1:23194165894:web:adc465443861c41d86a707"
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), initializeApp(firebaseConfig)]
 };
